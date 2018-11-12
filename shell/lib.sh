a520f989a922200e8ab47ce9a5cd3e7fa7e5d883
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-11-11 18:30:41 -0600

    fix func export

--- shell/lib.sh
+++ shell/lib.sh
@@ -4,13 +4,13 @@ set -o pipefail
 set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
 
-export -f error () {
+error () {
     echo "Error: $1"
     usage
     exit "${2}"
 } >&2
 
-export -f usage() {
+usage() {
 	cat <<END
 Overwrite usage function in shell script to provide help.
 Make sure you put the function after the library load.
@@ -18,4 +18,6 @@ END
 	exit 1
 }
 
+export -f error
+export -f usage
 export declare k_custom_lib_loaded=true
