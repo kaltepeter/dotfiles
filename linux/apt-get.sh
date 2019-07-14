4c694ab11192cb19d0944d4bb8ecfaa98c51a83e
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-14 12:48:33 -0500

    fix bug in lib load

--- linux/apt-get.sh
+++ linux/apt-get.sh
@@ -3,6 +3,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
+readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+[[ $(command -v k_custom_lib_loaded) ]] || source "${__dir}/../shell/lib.sh"
 
 typed_message 'CONFIG' "check for latest updates..."
 sudo apt-get update
