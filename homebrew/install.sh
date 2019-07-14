ba90f8eeab473948f4e2b47ef7fb9b5e6fc29678
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-14 11:33:37 -0500

    brew incompat

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -21,29 +21,28 @@ status "${BASH_SOURCE[0]} | ..."
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if test ! "$(command -v brew)"; then
-	typed_message 'INSTALL' "Homebrew"
+if test "$(uname)" = "Darwin"; then
+  # Check for Homebrew
+  if test ! "$(command -v brew)"; then
+    typed_message 'INSTALL' "Homebrew"
 
-	# Install the correct homebrew for each OS type
-	if test "$(uname)" = "Darwin"; then
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	# elif test [[ substr "$(uname -s)" 1 5 = "Linux" ]]; then
-	# 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    # Install the correct homebrew for each OS type
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # elif test [[ substr "$(uname -s)" 1 5 = "Linux" ]]; then
+    # 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   else
-    typed_message 'SKIP' "Brew. Not a Mac."
-	fi
+    typed_message 'SKIP' "brew already installed."
+    typed_message 'INSTALL' "Updating and cleaning brew."
+    update_brew
+    brew --version
+  fi
+
+  echo ''
+
+  xcode-select --install || typed_message 'SKIP' 'xcode already installed. Use softwareupdate.'
+  sh -c "${__dir}/brew.sh"
 else
-	typed_message 'SKIP' "brew already installed."
-  typed_message 'INSTALL' "Updating and cleaning brew."
-  update_brew
-  brew --version
+  typed_message 'SKIP' "Brew. Not a Mac."
 fi
-
-echo ''
-
-xcode-select --install || typed_message 'SKIP' 'xcode already installed. Use softwareupdate.'
-
-sh -c "${__dir}/brew.sh"
 
 exit 0
