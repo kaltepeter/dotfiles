b048d8a4528a56e26501a728f6c074bb71098cfb
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-10-15 22:30:09 -0500

    change order of ops

--- install.sh
+++ install.sh
@@ -5,8 +5,23 @@ set -o pipefail
 set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
 
+declare data_dir="${HOME}/data"
+if [[ -d "${data_dir}" ]]; then
+  typed_message 'SKIP' "${data_dir} exists."
+else
+  typed_message 'CREATE' "${data_dir}..."
+  mkdir "${data_dir}"
+fi
+
+git clone git@github.com:kaltepeter/dotfiles.git "${HOME}/data/"
+
+cd "${HOME}/data"
+
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 log_file="${__dir}/install.log"
+
+# shellcheck disable=SC1090
+[[ $(command -v k_custom_lib_loaded) ]] || source "${__dir}/shell/lib.sh"
 
 usage() {
   cat <<END
@@ -33,11 +48,11 @@ done
 shift $((OPTIND - 1))
 
 if [[ ! -f "${__dir}/.env" ]]; then
-  echo "CONFIG First run. ${__dir}/.env doesn't exist."
+  typed_message 'CONFIG' "First run. ${__dir}/.env doesn't exist."
 fi
 
 input="${__dir}/.env.example"
-echo "CONFIG writing inputs to ${__dir}/.env file"
+typed_message 'CONFIG' "writing inputs to ${__dir}/.env file"
 while IFS= read -r line; do
   [[ ${line} =~ ^#.*s ]] && continue
   # shellcheck disable=SC1001
@@ -51,22 +66,10 @@ done <"$input"
 # error "IMPORTANT: edit values and re-run bootstrap." 1
 
 set -o allexport
-echo "CONFIG setting vars from ${__dir}/.env"
+typed_message 'CONFIG' "setting vars from ${__dir}/.env"
 # shellcheck source=/dev/null
 source "${__dir}/.env"
 set +o allexport
-
-declare data_dir="${HOME}/data"
-if [[ -d "${data_dir}" ]]; then
-  echo "SKIP ${data_dir} exists."
-else
-  echo "CREATE ${data_dir}..."
-  mkdir "${data_dir}"
-fi
-
-git clone git@github.com:kaltepeter/dotfiles.git "${HOME}/data/"
-
-cd "${HOME}/data"
 
 sh "${__dir}/bootstrap.sh"
 
