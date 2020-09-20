20431f858b81c7941bf2defc2b713eab850f8242
kaltepeter <kayla.altepeter@datasite.com>
2020-09-20 11:11:26 -0500

    feat(alias): adding shortcuts for gif and git

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -11,3 +11,4 @@ export PATH=$PATH:/usr/local/opt/rabbitmq/sbin
 export PATH="/usr/local/sbin:$PATH"
 export TIMEFMT='%J %U user %S system %P cpu %*E total max RSS %M'
 export NODE_EX=":(exclude)*lock.json"
+export MOV_TO_GIF_DEFAULTS="fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
