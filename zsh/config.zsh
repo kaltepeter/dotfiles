872ed81f28f75c5289ace13fca464155457dfe5e
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-02-13 10:22:23 -0600

    chore(zsh): latest

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,10 +7,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH=$PATH:/usr/local/opt/rabbitmq/sbin
+export PATH=/usr/local/opt/rabbitmq/sbin:$PATH
 export PATH="/usr/local/sbin:$PATH"
 export TIMEFMT='%J %U user %S system %P cpu %*E total max RSS %M'
 export NODE_EX=":(exclude)*lock.json"
 export MOV_TO_GIF_DEFAULTS="fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
+# Use the python3 interpreter on your path
+export CLOUDSDK_PYTHON=python3
